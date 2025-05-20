edcf2593bedb8fd87afced3ee603f1b5ede4b546
Hieu Hoang <hieuht33@vingroup.net>
2025-05-20 17:44:48 +0700

    Add camera access demo and remove unused type definitions

--- dist/types/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Button } from './Button';
-import { ButtonOptions, ButtonLibrary } from './types';
-export { Button };
-export type { ButtonOptions, ButtonLibrary };
-declare const ButtonLib: ButtonLibrary;
-export default ButtonLib;
