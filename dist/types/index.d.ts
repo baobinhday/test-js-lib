4fe13091dbd88582ecd679fd4ba49e43dcee3f16
Hieu Hoang <hieuht33@vingroup.net>
2025-05-20 16:44:15 +0700

    Add button dist to repo

---
+++ dist/types/index.d.ts
@@ -0,0 +1,6 @@
+import { Button } from './Button';
+import { ButtonOptions, ButtonLibrary } from './types';
+export { Button };
+export type { ButtonOptions, ButtonLibrary };
+declare const ButtonLib: ButtonLibrary;
+export default ButtonLib;
